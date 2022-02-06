f3991fec623d9e919fa663af7a8b91cd1de6e462
Anasmi13 <98353073+Anasmi13@users.noreply.github.com>
2022-02-07 00:12:48 +0500

    Дефолтные значения в полях

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("Тестовая", "Тестовая2", "Тестовая3"));
+        app.getGroupHelper().fillGroupForm(new GroupData("Тестовая", null, null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnGroupPage();
     }
