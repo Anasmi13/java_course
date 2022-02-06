85b00022a60f7aa6129a650dc496dd3e9500690d
Anasmi13 <98353073+Anasmi13@users.noreply.github.com>
2022-02-07 01:37:55 +0500

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -10,7 +10,7 @@ public class ContactCreationTests extends TestBase {
     app.getContactHelper().initContactCreation();
     app.getContactHelper().fillContactForm(new ContactData("Сергей", "Александрович", "Меньшов", "Рога и копыта", "г. Москва, ул. Советская 4, офис 410", "89456582355", "menshov_am_85@mail.ru", "15", "April", "1985", "Тестовая","г. Москва ул. Невельская, кв.306"), true);
     app.getContactHelper().submitContactCreation();
-    app.getContactHelper().returnHomePage();
+    app.getNavigationHelper().gotoHomePage();
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -10,7 +10,7 @@ public class ContactModificationTests extends TestBase {
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData("Александр", "Сергеевич", "Меньшов", "Рога и копыта", "г. Москва, ул. Советская 4, офис 410", "89456582355", "menshov_am_85@mail.ru", "15", "April", "1985", null,"г. Москва ул. Невельская, кв.306"), false);
         app.getContactHelper().submitContactModification();
-        app.getContactHelper().returnHomePage();
+        app.getNavigationHelper().gotoHomePage();
     }
 
 }
