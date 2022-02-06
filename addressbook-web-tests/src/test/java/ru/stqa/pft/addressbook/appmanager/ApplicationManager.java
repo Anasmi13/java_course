043cc9b0c5c4ccf7b92102407d5f593cfed2634b
Anasmi13 <98353073+Anasmi13@users.noreply.github.com>
2022-02-07 00:05:22 +0500

    Добавлено 7 задание

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,6 +3,12 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.BrowserType;
+
 
 import java.time.Duration;
 
@@ -13,10 +19,26 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private ContactHelper contactHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "C:/Windows/System32/chromedriver.exe");
-        wd = new ChromeDriver();
+
+        if (browser.equals(BrowserType.CHROME)) {
+            System.setProperty("webdriver.chrome.driver", "C:/Windows/System32/chromedriver.exe");
+            wd = new ChromeDriver();
+
+        } else if (browser.equals(BrowserType.OPERA)) {
+            System.setProperty("webdriver.opera.driver", "C:/Windows/System32/operadriver.exe");
+            wd = new OperaDriver();
+
+        } else if (browser.equals(BrowserType.EDGE)) {
+            System.setProperty("webdriver.edge.driver", "C:/Windows/System32/msedgedriver.exe");
+            wd = new EdgeDriver();
+        }
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
